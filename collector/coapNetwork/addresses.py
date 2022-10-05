17bfd8ef6fe4685c357456ce66681780322941d6
Mickey374 <asantemichael37483@gmail.com>
2022-10-05 20:05:11 +0200

    Add list collection for Fans within Address

--- collector/coapNetwork/addresses.py
+++ collector/coapNetwork/addresses.py
@@ -1,9 +1,13 @@
 class Addresses:
     ad_Filters = []
+    ad_Fans = []
 
     def insertNewAddress(source, val):
         if val == "filters":
             Addresses.ad_Filters.append(source)
+        elif val == "fans":
+            Addresses.ad_Fans.append(source)
+
     
     def constructAddress():
         if Addresses.address is not None:
