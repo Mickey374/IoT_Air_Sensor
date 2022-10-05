ee5b3c628449d3db71b3ebfc5d1e3514827fc1c9
Mickey374 <asantemichael37483@gmail.com>
2022-10-05 20:28:26 +0200

    Added the parameter for the fan.

--- collector/globalStatus.py
+++ collector/globalStatus.py
@@ -1,6 +1,10 @@
 class globalStatus:
     statusFilter = 0
+    statusFan = 0
     changeVal = 0
 
     def setFilterStatus(status):
         globalStatus.statusFilter = status
+
+    def setFanStatus(status):
+        globalStatus.statusFan = status
