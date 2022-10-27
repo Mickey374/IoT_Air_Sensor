2745dbf0d04d6f86904aba1ff5acf8874df59177
Mickey374 <asantemichael37483@gmail.com>
2022-10-27 19:19:04 +0200

    Initialize the Observer sensor class

--- collector/coapNetwork/sensor.py
+++ collector/coapNetwork/sensor.py
@@ -11,5 +11,18 @@ from mqttNetwork.value_collector import MqttClientProfile
 from database.db import Database
 from globalStatus import globalStatus
 
-class ObserverSensor():
+class ObserveSensor:
+
+    def __init__(self, resource, source_address, type):
+        self.db = Database()
+        self.connection = self.db.connect()
+        self.address = source_address
+        self.resource = resource
+        self.type = type
+        self.mqtt = None
+        self.startObserving()
     
+
+    def startObserving(self):
+        
+
