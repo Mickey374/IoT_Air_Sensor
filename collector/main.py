c17d12893f3304d2917ab857f67f685fb7ef17be
Mickey374 <asantemichael37483@gmail.com>
2022-11-15 16:28:16 +0100

    Completed command for simulate.

--- collector/main.py
+++ collector/main.py
@@ -24,56 +24,79 @@ class CoAPServer(CoAP):
         CoAP.__init__(self, (host, port), False)
         self.add_resource("registry", ResExample())
     
-    def allCommands():
-        print(">> ALL COMMANDS TO START \n")
-        print(
-        "help \n"\
-        "activate\n"\
-        "logs\n"\
-        "simulate\n"\
-        "change params\n"
-        "exit\n\n")
+def allCommands():
+    print(">> ALL COMMANDS TO START \n")
+    print(
+    "help \n"\
+    "activate\n"\
+    "filter\n" \
+    "logs\n"\
+    "simulate\n"\
+    "change params\n"
+    "exit\n\n")
+
+def getValuesFromClient(client, client1):
+    level = client1.levIn if client1.levIn else 50
+    humidity = client.humIn if client.humIn else 60
+    temperature = client.tempIn if client.tempIn else 27
+    co2 = client.co2In if client.co2In else 1300
+    tempOut = client.tempOut if client.tempOut else 30
+
+    return [level, humidity, temperature, co2, tempOut]
+
+def checkUserCommand(command, client, client1):
+    if command == "help":
+        showInfo()
     
-    def getValuesFromClient(client, client1):
-        level = client1.levIn if client1.levIn else 50
-        humidity = client.humIn if client.humIn else 60
-        temperature = client.tempIn if client.tempIn else 27
-        co2 = client.co2In if client.co2In else 1300
-        tempOut = client.tempOut if client.tempOut else 30
+    elif command == "logs":
+        try:
+            msg = str(client.message)
+            print("\n>>Press Ctrl+C to terminate session \n")
+            while True:
+                time.sleep(1)
+                if(str(client.message) != msg):
+                    print("\n>>"+str(client.message)+ str(client1.message))
+                    msg = str(client.message)
+        except KeyboardInterrupt:
+            return
 
-        return [level, humidity, temperature, co2, tempOut]
-
-    def checkUserCommand(command, client, client1):
-        if command == "help":
-            showInfo()
-        
-        elif command == "log":
-            try:
-                msg = str(client.message)
-                print("\n>>Press Ctrl+C to terminate session \n")
-                while True:
-                    time.sleep(1)
-                    if(str(client.message) != msg):
-                        print("\n>>"+str(client.message)+ str(client1.message))
-                        msg = str(client.message)
-            except KeyboardInterrupt:
-                return
-
-        elif command == "filter":
-            try:
-                msg = str(client.message)
-                print("\n>>Press Ctrl+C to terminate session \n")
-                while True:
-                    time.sleep(1)
-                    if(str(client1.message) != msg):
-                        print("\n>>"+str(client1.message))
-                        msg = str(client1.message)
-            except KeyboardInterrupt:
-                return   
-        
-        
-
-
-
-
+    elif command == "filter":
+        try:
+            msg = str(client.message)
+            print("\n>>Press Ctrl+C to terminate session \n")
+            while True:
+                time.sleep(1)
+                if(str(client1.message) != msg):
+                    print("\n>>"+str(client1.message))
+                    msg = str(client1.message)
+        except KeyboardInterrupt:
+            return   
     
+    elif command == "activate":
+        client.communicateToSensors("start", "inValues")
+
+    elif command == "simulate":
+        try:
+            data = getValuesFromClient(client, client1)
+            while True:
+                # time.sleep(1)
+                statFans = str(globalStatus.setFanStatus)
+                statFilters = str(globalStatus.setFilterStatus)
+                data = getValuesFromClient(client, client1)
+                # >>Clear the terminal
+                os.system('cls' if os.name == 'nt' else 'clear')
+                print(pa.airSensor[statFans][statFilters].format(data[0],data[1],data[2],data[3],data[4]))
+                print("\n>>Press Ctrl+C to exit\n")
+                time.sleep(1)
+
+        
+        except KeyboardInterrupt:
+            return
+    
+    
+
+
+
+
+def showInfo():
+    return 1
