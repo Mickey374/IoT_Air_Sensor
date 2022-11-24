4d210404834eaa094772a31d421f3f1d5364326b
Mickey374 <asantemichael37483@gmail.com>
2022-11-24 12:22:29 +0100

    Completed function for update_gas monitoring

--- collector/mqttNetwork/filter_extractor.py
+++ collector/mqttNetwork/filter_extractor.py
@@ -11,4 +11,13 @@ from globalStatus import globalStatus
 class MqttClientExtractionFilter:
     def on_connect(self, client, userdata, flags, rc):
         self.client.subscribe("status_gasExtractor")
-        self.client.subscribe("actuator__gasExtractor")
+        self.client.subscribe("actuator_gasExtractor")
+    
+    def update_gas_monitoring_status(self, ad, status):
+        dt = datetime.now()
+        cursor = self.connection.cursor()
+        query = "INSERT INTO `actuator_filtering` (`address`, `timestamp`, `status`) VALUES (%s, %s, %s)"
+        cursor.execute(query, (str(ad), dt, status))
+        print("\nSTATUS = "+ status)
+        self.connection.commit()
+    
